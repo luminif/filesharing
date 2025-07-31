d0b92f28ff8df827e4674790b563414386109438
luminif <arcocs72717@mail.ru>
2025-07-31 17:16:47 +0300

    Initial commit

---
+++ src/main/java/ru/java/filesharing/FilesharingApplication.java
@@ -0,0 +1,13 @@
+package ru.java.filesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesharingApplication.class, args);
+	}
+
+}
